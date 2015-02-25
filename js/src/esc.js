dab0d35f24893633b18624989dc4b7e4208e343c
MAYA Ma <sesemaya@gmail.com>
2015-02-25 15:59:38 +1100

    gh-pages update to page palettes

--- js/src/esc.js
+++
@@ -1,9 +0,0 @@
-// close menu and/or tile if esc key is pressed
-	$(document).keyup(function(e) {
-		if (e.which == '27') {
-			tReset();
-			if ($('body').hasClass('menu-open')) {
-				mReset();
-			};
-		};
-	});
