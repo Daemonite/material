b500afd3446ff5c9bd9dc4c6481f15f9b396b721
MAYA Ma <sesemaya@gmail.com>
2015-03-02 14:28:50 +1100

    add floating labels

--- js/base.js
+++ js/base.js
@@ -40,14 +40,41 @@ if(k&&j[k]&&(e||j[k].data)||void 0!==d||"string"!=typeof b)return k||(k=i?a[h]=c
 	on_resize(function() {
 		$('body').css('margin-bottom', $('.footer').outerHeight());
 	})();
-// checkbox
+// checkbox & radio
 	$('.checkbox-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span><span class="circle-icon icon icon-done"></span>');
 	});
 
-// radio
 	$('.radio-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span>');
+	});
+
+// floating-label
+	function floatingLabel(input) {
+		var parent = input.closest('.form-group-label');
+		if(input.val()) {
+			parent.addClass('control-highlight');
+		} else {
+			parent.removeClass('control-highlight');
+		}
+	}
+
+	$(function() {
+		var allchecked;
+		if($('.form-group-label').length) {
+			$('.form-group-label .form-control').each(function() {
+				floatingLabel($(this));
+			});
+		}
+		$('.form-group-label .form-control').on('change', function() {
+			floatingLabel($(this));
+		});
+		$('.form-group-label .form-control').on('focusin', function() {
+			$(this).closest('.form-group-label').addClass('control-focus');
+		});
+		$('.form-group-label .form-control').on('focusout', function() {
+			$(this).closest('.form-group-label').removeClass('control-focus');
+		});
 	});
 // header affix
 	var $header = $('.header');
--- js/src/form-adv.js
+++ js/src/form-adv.js
@@ -1,9 +1,36 @@
-// checkbox
+// checkbox & radio
 	$('.checkbox-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span><span class="circle-icon icon icon-done"></span>');
 	});
 
-// radio
 	$('.radio-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span>');
+	});
+
+// floating-label
+	function floatingLabel(input) {
+		var parent = input.closest('.form-group-label');
+		if(input.val()) {
+			parent.addClass('control-highlight');
+		} else {
+			parent.removeClass('control-highlight');
+		}
+	}
+
+	$(function() {
+		var allchecked;
+		if($('.form-group-label').length) {
+			$('.form-group-label .form-control').each(function() {
+				floatingLabel($(this));
+			});
+		}
+		$('.form-group-label .form-control').on('change', function() {
+			floatingLabel($(this));
+		});
+		$('.form-group-label .form-control').on('focusin', function() {
+			$(this).closest('.form-group-label').addClass('control-focus');
+		});
+		$('.form-group-label .form-control').on('focusout', function() {
+			$(this).closest('.form-group-label').removeClass('control-focus');
+		});
 	});
