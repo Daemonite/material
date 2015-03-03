bf9382b26952e5f03c94d170ac7454a7f2c02a41
MAYA Ma <sesemaya@gmail.com>
2015-03-03 16:13:28 +1100

    update form styles

--- js/base.js
+++ js/base.js
@@ -47,12 +47,27 @@ if(k&&j[k]&&(e||j[k].data)||void 0!==d||"string"!=typeof b)return k||(k=i?a[h]=c
 	$('.checkbox-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span><span class="circle-icon icon icon-done"></span>');
 	});
-
 	$('.radio-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span>');
 	});
 
-// floating-label
+// floating label
+	var allchecked;
+	if($('.form-group-label').length) {
+		$('.form-group-label .form-control').each(function() {
+			floatingLabel($(this));
+		});
+	}
+	$('.form-group-label .form-control').on('change', function() {
+		floatingLabel($(this));
+	});
+	$('.form-group-label .form-control').on('focusin', function() {
+		$(this).closest('.form-group-label').addClass('control-focus');
+	});
+	$('.form-group-label .form-control').on('focusout', function() {
+		$(this).closest('.form-group-label').removeClass('control-focus');
+	});
+
 	function floatingLabel(input) {
 		var parent = input.closest('.form-group-label');
 		if(input.val()) {
@@ -62,22 +77,12 @@ if(k&&j[k]&&(e||j[k].data)||void 0!==d||"string"!=typeof b)return k||(k=i?a[h]=c
 		}
 	}
 
-	$(function() {
-		var allchecked;
-		if($('.form-group-label').length) {
-			$('.form-group-label .form-control').each(function() {
-				floatingLabel($(this));
-			});
-		}
-		$('.form-group-label .form-control').on('change', function() {
-			floatingLabel($(this));
-		});
-		$('.form-group-label .form-control').on('focusin', function() {
-			$(this).closest('.form-group-label').addClass('control-focus');
-		});
-		$('.form-group-label .form-control').on('focusout', function() {
-			$(this).closest('.form-group-label').removeClass('control-focus');
-		});
+// icon label
+	$('.form-group-icon .form-control').on('focusin', function() {
+		$(this).closest('.form-group-icon').addClass('control-focus');
+	});
+	$('.form-group-icon .form-control').on('focusout', function() {
+		$(this).closest('.form-group-icon').removeClass('control-focus');
 	});
 
 // textarea autosize
--- js/src/form-adv.js
+++ js/src/form-adv.js
@@ -2,12 +2,27 @@
 	$('.checkbox-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span><span class="circle-icon icon icon-done"></span>');
 	});
-
 	$('.radio-adv').each(function() {
 		$('label', $(this)).append('<span class="circle"></span><span class="circle-check"></span>');
 	});
 
-// floating-label
+// floating label
+	var allchecked;
+	if($('.form-group-label').length) {
+		$('.form-group-label .form-control').each(function() {
+			floatingLabel($(this));
+		});
+	}
+	$('.form-group-label .form-control').on('change', function() {
+		floatingLabel($(this));
+	});
+	$('.form-group-label .form-control').on('focusin', function() {
+		$(this).closest('.form-group-label').addClass('control-focus');
+	});
+	$('.form-group-label .form-control').on('focusout', function() {
+		$(this).closest('.form-group-label').removeClass('control-focus');
+	});
+
 	function floatingLabel(input) {
 		var parent = input.closest('.form-group-label');
 		if(input.val()) {
@@ -17,22 +32,12 @@
 		}
 	}
 
-	$(function() {
-		var allchecked;
-		if($('.form-group-label').length) {
-			$('.form-group-label .form-control').each(function() {
-				floatingLabel($(this));
-			});
-		}
-		$('.form-group-label .form-control').on('change', function() {
-			floatingLabel($(this));
-		});
-		$('.form-group-label .form-control').on('focusin', function() {
-			$(this).closest('.form-group-label').addClass('control-focus');
-		});
-		$('.form-group-label .form-control').on('focusout', function() {
-			$(this).closest('.form-group-label').removeClass('control-focus');
-		});
+// icon label
+	$('.form-group-icon .form-control').on('focusin', function() {
+		$(this).closest('.form-group-icon').addClass('control-focus');
+	});
+	$('.form-group-icon .form-control').on('focusout', function() {
+		$(this).closest('.form-group-icon').removeClass('control-focus');
 	});
 
 // textarea autosize
