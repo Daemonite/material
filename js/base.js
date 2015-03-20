21964eeb6bcdd96d8caa1c25428e6c85a2a0a799
MAYA Ma <sesemaya@gmail.com>
2015-03-20 14:37:55 +1100

    update tab

--- js/base.js
+++ js/base.js
@@ -246,26 +246,35 @@ $('.datepicker-adv-default').each(function(index) {
 		ghostClass: 'sortable-ghost',
 		handle: '.sortable-handle'
 	});
-// tab index
+// tab indicator
 	$('.tab-nav').each(function() {
-		$('a[data-toggle="tab"]', $(this)).each(function(index) {
-			$(this).attr('data-tab-index', index);
-		});
+		$(this).append('<div class="tab-nav-indicator"></div>');
+		tabSwitch($('.nav > li.active', $(this)), null);
 	});
 
 // tab switch
-	$('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
-		var $newTab = $(e.target),
-		    $oldTab = $(e.relatedTarget);
-		if ($newTab.attr('data-tab-index') < $oldTab.attr('data-tab-index')) {
-			$newTab.addClass('from-right');
-			$oldTab.addClass('to-left');
-			setTimeout(function() {
-				$newTab.removeClass('from-right');
-				$oldTab.removeClass('to-left');
-			}, 300);
-		};
+	$('.tab-nav a[data-toggle="tab"]').on('show.bs.tab', function(e) {
+	 	tabSwitch($(e.target), $(e.relatedTarget));
 	});
+
+	function tabSwitch(newTab, oldTab) {
+		var $nav = newTab.closest('.tab-nav'),
+		    $navIndicator = $('.tab-nav-indicator', $nav),
+		    navOffset = $nav.offset().left,
+	 	    navWidth = $nav.width(),
+	 	    newTabOffset = newTab.offset().left,
+	 	    newTabWidth = newTab.outerWidth();
+		if (oldTab != null && oldTab.offset().left > newTabOffset) {
+			$navIndicator.addClass('reverse');
+			setTimeout(function() {
+				$navIndicator.removeClass('reverse');
+			}, 450);
+		};
+	 	$navIndicator.css({
+	 		left: (newTabOffset - navOffset),
+	 		right: navOffset + navWidth - newTabOffset - newTabWidth
+	 	});
+	}
 // tile close
 	function tReset() {
 		$('.tile-collapse.active .tile-active-show').collapse('hide');
