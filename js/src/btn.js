90fac9bafaa7cdcd1160ca8bada0fd53b5a28edd
MAYA Ma <sesemaya@gmail.com>
2015-03-31 15:30:36 +1100

    add button waves

--- js/src/btn.js
+++ js/src/btn.js
@@ -1,11 +1,4 @@
-// btn active
-	$(document).on('mousedown touchstart', '.btn', function() {
-		var $this = $(this);
-
-		if (!$this.hasClass('btn-active')) {
-			$this.addClass('btn-active');
-			setTimeout(function() {
-				$this.removeClass('btn-active');
-			}, 450);
-		}
-	})
+// btn waves
+	Waves.displayEffect({
+		duration: 900
+	});
