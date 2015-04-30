b99a33e4e726560735d10ed6ab3c192d993245a5
MAYA Yang Ma <sesemaya@gmail.com>
2015-04-30 12:42:22 +1000

    load robot font using webfontloader.js and update related css and js

--- js/src/content.js
+++ js/src/content.js
@@ -1,6 +1,10 @@
 // fixed left/right hand side column affix
-	var footerHeight = 0,
-	    htmlHeight = 0;
+	var contentPadding = 0,
+	    footerOffset = 0;
+
+	if ($('.content').length) {
+		contentPadding = parseInt($('.content').css('padding-bottom').replace('px', ''));
+	};
 
 	$(window).on('scroll', function() {
 		$('.content-fix').each(function(index) {
@@ -13,14 +17,18 @@
 	function contentFix(content) {
 		var scrolled = window.innerHeight + window.pageYOffset;
 
+		if ($('.footer').length) {
+
+		};
+
 		if (window.pageYOffset >= (content.offset().top - headerHeight)) {
 			if ((content.is('[class*="col-xx"]')) || (content.is('[class*="col-xs"]') && $(window).width() >= 480) || (content.is('[class*="col-sm"]') && $(window).width() >= 768) || (content.is('[class*="col-md"]') && $(window).width() >= 992) || (content.is('[class*="col-lg"]') && $(window).width() >= 1440)) {
 				if (!content.hasClass('fixed')) {
 					content.addClass('fixed');
 					$('.content-fix-wrap', content).scrollTop(0);
 				};
-				if (htmlHeight <= scrolled) {
-					$('.content-fix-inner', content).css('padding-bottom', scrolled - htmlHeight);
+				if (footerOffset != 0 && footerOffset <= scrolled) {
+					$('.content-fix-inner', content).css('padding-bottom', (scrolled - footerOffset + contentPadding));
 				};
 			};
 		} else {
@@ -31,14 +39,12 @@
 
 // fixed left/right hand side column padding bottom and width
 	function contentFixPushCal() {
-		htmlHeight = $('body').height();
-
 		$('.content-fix-scroll').each(function(index) {
 			$(this).css('width', $(this).closest('.content-fix').outerWidth());
 			$('.content-fix-inner', $(this)).css('width', $(this).closest('.content-fix').width());
 		});
 
 		if ($('.footer').length) {
-			footerHeight = $('.footer').outerHeight();
-		}
+			footerOffset = $('.footer').offset().top;
+		};
 	}
---
+++ js/src/webfont.js
@@ -0,0 +1,28 @@
+// webfont.js
+	WebFontConfig = {
+		classes: false,
+		fontactive: function() {
+			// fixed left/right hand side column padding bottom and width
+				if ($('.content-fix').length) {
+					contentFixPushCal();
+				};
+
+			// footer push
+				footerPush();
+
+			// header height
+				headerHeightCal();
+		},
+		google: {
+			families: ['Roboto:300,300italic,400,400italic,700,700italic']
+		}
+	};
+
+	(function() {
+		var wf = document.createElement('script');
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.10/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	})();
