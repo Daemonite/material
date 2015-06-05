3e8d812b8f447f111ea617b8cabcdcea417d63f3
MAYA Yang Ma <sesemaya@gmail.com>
2015-06-05 14:24:54 +1000

    separate jquery from material base js

--- js/src/webfont.js
+++ js/src/webfont.js
@@ -33,7 +33,7 @@
 
 	(function() {
 		var wf = document.createElement('script');
-		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.10/webfont.js';
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
 		wf.type = 'text/javascript';
 		wf.async = 'true';
 		var s = document.getElementsByTagName('script')[0];
