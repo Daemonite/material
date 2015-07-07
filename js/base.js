bf59eea30ea5f579cb675ec66c855a9f3884f57e
MAYA Yang Ma <sesemaya@gmail.com>
2015-07-07 14:01:00 +1000

    remove setting max-width on dropdown menu js

--- js/base.js
+++ js/base.js
@@ -126,23 +126,6 @@
 		};
 	}(jQuery);
 
-// dropdown menu max width
-	$(document).on('show.bs.dropdown', '.dropdown', function () {
-		var $dropdownMenu = $('.dropdown-menu', $(this)),
-		    $dropdownToggle = $('[class*="dropdown-toggle"]', $(this)),
-		    dropdownWidth;
-
-		if ($dropdownMenu.length && $dropdownToggle.length) {
-			if ($dropdownMenu.hasClass('dropdown-menu-right') || $dropdownMenu.parents('.dropdown.pull-right').length || $dropdownMenu.parents('.nav.pull-right').length) {
-				dropdownWidth = $dropdownToggle.offset().left + $dropdownToggle.outerWidth();
-			} else {
-				dropdownWidth = window.innerWidth - $dropdownToggle.offset().left;
-			}
-
-			$dropdownMenu.css('max-width', dropdownWidth);
-		};
-	});
-
 // close menu and/or tile if esc key is pressed
 	$(document).keyup(function(e) {
 		if (e.which == '27') {
