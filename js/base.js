015e0acfbde3ee4bbac130f1fa9bcc0641048ea5
MAYA Yang Ma <sesemaya@gmail.com>
2015-07-09 10:24:30 +1000

    separate auto expanding textarea js from floating label js

--- js/base.js
+++ js/base.js
@@ -159,30 +159,36 @@
 						};
 				};
 			};
+
+			return this;
 		};
 	}(jQuery));
 
-	$('.form-group-label .form-control').each(function() {
-		$(this).floatingLabel('change');
-	});
+	$(function () {
+		$('.form-group-label .form-control').each(function() {
+			$(this).floatingLabel('change');
+		});
 
-	$(document).on('change', '.form-group-label .form-control', function() {
-		$(this).floatingLabel('change');
-	});
+		$(document).on('change', '.form-group-label .form-control', function() {
+			$(this).floatingLabel('change');
+		});
 
-	$(document).on('focusin', '.form-group-label .form-control', function() {
-		$(this).floatingLabel('focusin');
-	});
+		$(document).on('focusin', '.form-group-label .form-control', function() {
+			$(this).floatingLabel('focusin');
+		});
 
-	$(document).on('focusout', '.form-group-label .form-control', function() {
-		$(this).floatingLabel('focusout');
+		$(document).on('focusout', '.form-group-label .form-control', function() {
+			$(this).floatingLabel('focusout');
+		});
 	});
 
 // textarea autosize v0.4.0
 // Javier Julio: https://github.com/javierjulio/textarea-autosize
 	!function(t,e){function i(e){this.element=e,this.$element=t(e),this.init()}var n="textareaAutoSize",h="plugin_"+n,s=function(t){return t.replace(/\s/g,"").length>0};i.prototype={init:function(){var i=(this.$element.outerHeight(),parseInt(this.$element.css("paddingBottom"))+parseInt(this.$element.css("paddingTop")));s(this.element.value)&&this.$element.height(this.element.scrollHeight-i),this.$element.on("input keyup",function(){var n=t(e),h=n.scrollTop();t(this).height(0).height(this.scrollHeight-i),n.scrollTop(h)})}},t.fn[n]=function(e){return this.each(function(){t.data(this,h)||t.data(this,h,new i(this,e))}),this}}(jQuery,window,document);
 
-	$('.textarea-autosize').textareaAutoSize();
+	$(function () {
+		$('.textarea-autosize').textareaAutoSize();
+	});
 
 // get target from trigger
 	getTargetFromTrigger = function(trigger) {
