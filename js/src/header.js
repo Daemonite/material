f673a38eda0c71be05113b80bdd60b1d5b1783e6
MAYA Yang Ma <sesemaya@gmail.com>
2015-07-14 13:38:42 +1000

    add additional header styles based on material guideline

--- js/src/header.js
+++ js/src/header.js
@@ -1,13 +1,11 @@
 // header waterfall
 	$(function () {
-		$(window).on('scroll', function() {
-			$('.header-waterfall').each(function () {
-				var $this = $(this);
+		'use strict';
 
-				if (window.pageYOffset > 0) {
-					$this.addClass('header-waterfall-fixed');
-				} else {
-					$this.removeClass('header-waterfall-fixed');
+		$('.header-waterfall').each(function () {
+			$(this).affix({
+				offset: {
+					top: 1
 				}
 			});
 		});
