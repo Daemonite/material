91a33b9d1ea736148406377563ccdfc80bf7c67f
MAYA Yang Ma <sesemaya@gmail.com>
2015-07-16 13:20:20 +1000

    update menu.js (plus related css changes)

--- js/src/get-target.js
+++
@@ -1,8 +0,0 @@
-// get target from trigger
-	getTargetFromTrigger = function(trigger) {
-		var href;
-		var target = trigger.attr('data-target')
-		    || (href = trigger.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '');
-
-		return target;
-	}
