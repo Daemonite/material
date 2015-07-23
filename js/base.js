e7d5c373667ea2a8d836cb8ac641a378b1b65416
MAYA Yang Ma <sesemaya@gmail.com>
2015-07-23 14:54:25 +1000

    update menu.js syntax

--- js/base.js
+++ js/base.js
@@ -150,15 +150,15 @@
 		'use strict';
 
 		var Menu = function (element, options) {
+			this.ignoreBackdropClick = false;
+			this.isShown             = null;
 			this.options             = options;
+			this.originalBodyPad     = null;
+			this.scrollbarWidth      = 0;
+			this.$backdrop           = null;
 			this.$body               = $(document.body);
 			this.$element            = $(element);
 			this.$dialog             = this.$element.find('.menu-scroll');
-			this.$backdrop           = null;
-			this.isShown             = null;
-			this.originalBodyPad     = null;
-			this.scrollbarWidth      = 0;
-			this.ignoreBackdropClick = false;
 		};
 
 		if (!$.fn.modal) {
@@ -170,6 +170,86 @@
 		Menu.TRANSITION_DURATION_BACKDROP = 150;
 
 		Menu.prototype = $.extend({}, $.fn.modal.Constructor.prototype);
+
+		Menu.prototype.backdrop = function (callback) {
+			var that = this;
+
+			if (this.isShown && this.options.backdrop) {
+				var doAnimate = $.support.transition;
+
+				this.$backdrop = $(document.createElement('div')).addClass('menu-backdrop').appendTo(this.$body);
+
+				this.$element.on('click.dismiss.bs.menu', $.proxy(function (e) {
+					if (this.ignoreBackdropClick) {
+						this.ignoreBackdropClick = false;
+						return;
+					};
+
+					if (e.target !== e.currentTarget) {
+						return;
+					};
+
+					this.options.backdrop == 'static' ? this.$element[0].focus() : this.hide();
+				}, this));
+
+				if (doAnimate) {
+					this.$backdrop[0].offsetWidth;
+				};
+
+				this.$backdrop.addClass('in');
+
+				if (!callback) {
+					return;
+				};
+
+				doAnimate ? this.$backdrop.one('bsTransitionEnd', callback).emulateTransitionEnd(Menu.TRANSITION_DURATION_BACKDROP) : callback();
+			} else if (!this.isShown && this.$backdrop) {
+				this.$backdrop.removeClass('in');
+
+				var callbackRemove = function () {
+					that.removeBackdrop();
+					callback && callback();
+				};
+
+				$.support.transition ? this.$backdrop.one('bsTransitionEnd', callbackRemove).emulateTransitionEnd(Menu.TRANSITION_DURATION_BACKDROP) : callbackRemove();
+			} else if (callback) {
+				callback();
+			};
+		};
+
+		Menu.prototype.hide = function (e) {
+			if (e) e.preventDefault();
+
+			e = $.Event('hide.bs.menu');
+
+			this.$element.trigger(e);
+
+			if (!this.isShown || e.isDefaultPrevented()) {
+				return;
+			};
+
+			this.isShown = false;
+
+			this.escape();
+
+			$(document).off('focusin.bs.modal');
+
+			this.$element.removeClass('in').off('click.dismiss.bs.menu').off('mouseup.dismiss.bs.menu');
+
+			this.$dialog.off('mousedown.dismiss.bs.menu');
+
+			$.support.transition ? this.$element.one('bsTransitionEnd', $.proxy(this.hideModal, this)).emulateTransitionEnd(Menu.TRANSITION_DURATION) : this.hideModal();
+		};
+
+		Menu.prototype.hideModal = function () {
+			var that = this;
+
+			this.$element.hide();
+
+			this.backdrop(function () {
+				that.$element.trigger('hidden.bs.menu');
+			});
+		};
 
 		Menu.prototype.show = function (_relatedTarget) {
 			var that = this;
@@ -220,86 +300,6 @@
 			});
 		};
 
-		Menu.prototype.hide = function (e) {
-			if (e) e.preventDefault();
-
-			e = $.Event('hide.bs.menu');
-
-			this.$element.trigger(e);
-
-			if (!this.isShown || e.isDefaultPrevented()) {
-				return;
-			};
-
-			this.isShown = false;
-
-			this.escape();
-
-			$(document).off('focusin.bs.modal');
-
-			this.$element.removeClass('in').off('click.dismiss.bs.menu').off('mouseup.dismiss.bs.menu');
-
-			this.$dialog.off('mousedown.dismiss.bs.menu');
-
-			$.support.transition ? this.$element.one('bsTransitionEnd', $.proxy(this.hideModal, this)).emulateTransitionEnd(Menu.TRANSITION_DURATION) : this.hideModal();
-		};
-
-		Menu.prototype.hideModal = function () {
-			var that = this;
-
-			this.$element.hide();
-
-			this.backdrop(function () {
-				that.$element.trigger('hidden.bs.menu');
-			});
-		};
-
-		Menu.prototype.backdrop = function (callback) {
-			var that = this;
-
-			if (this.isShown && this.options.backdrop) {
-				var doAnimate = $.support.transition;
-
-				this.$backdrop = $(document.createElement('div')).addClass('menu-backdrop').appendTo(this.$body);
-
-				this.$element.on('click.dismiss.bs.menu', $.proxy(function (e) {
-					if (this.ignoreBackdropClick) {
-						this.ignoreBackdropClick = false;
-						return;
-					};
-
-					if (e.target !== e.currentTarget) {
-						return;
-					};
-
-					this.options.backdrop == 'static' ? this.$element[0].focus() : this.hide();
-				}, this));
-
-				if (doAnimate) {
-					this.$backdrop[0].offsetWidth;
-				};
-
-				this.$backdrop.addClass('in');
-
-				if (!callback) {
-					return;
-				};
-
-				doAnimate ? this.$backdrop.one('bsTransitionEnd', callback).emulateTransitionEnd(Menu.TRANSITION_DURATION_BACKDROP) : callback();
-			} else if (!this.isShown && this.$backdrop) {
-				this.$backdrop.removeClass('in');
-
-				var callbackRemove = function () {
-					that.removeBackdrop();
-					callback && callback();
-				};
-
-				$.support.transition ? this.$backdrop.one('bsTransitionEnd', callbackRemove).emulateTransitionEnd(Menu.TRANSITION_DURATION_BACKDROP) : callbackRemove();
-			} else if (callback) {
-				callback();
-			};
-		}
-
 		function Plugin (option, _relatedTarget) {
 			return this.each(function () {
 				var $this   = $(this);
@@ -310,7 +310,7 @@
 				if (typeof option == 'string') data[option](_relatedTarget);
 				else if (options.show) data.show(_relatedTarget);
 			});
-		}
+		};
 
 		var old = $.fn.menu;
 
@@ -320,7 +320,7 @@
 		$.fn.menu.noConflict = function () {
 			$.fn.menu = old;
 			return this;
-		}
+		};
 
 		$(document).on('click.bs.menu.data-api', '[data-toggle="menu"]', function (e) {
 			var $this   = $(this);
@@ -340,7 +340,7 @@
 				$target.one('hidden.bs.menu', function () {
 					$this.is(':visible') && $this.trigger('focus');
 				});
-			})
+			});
 
 			Plugin.call($target, option, this);
 		});
