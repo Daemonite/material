3d3981adb52b7adeee80bdbf55ba2492cee90cf8
MAYA Ma <yang@daemon.com.au>
2016-04-07 12:07:29 +1000

    fix "google is undefined" error on page without google maps loaded

--- js/project.js
+++ js/project.js
@@ -20,95 +20,95 @@
 		});
 	};
 
-	initMap();
+	if (typeof google != 'undefined') {
+		initMap();
+	};
 
-	// jquery ui
-		if (typeof jQuery.ui != 'undefined') {
-			// draggable
-				$('.ui-picker-draggable-handler').draggable({
-					addClasses: false,
-					appendTo: 'body',
-					cursor: 'move',
-					cursorAt: {
-						top: 0,
-						left: 0 
-					},
-					delay: 100,
-					helper: function () {
-						return $('<div class="tile tile-brand-accent ui-draggable-helper"><div class="tile-side pull-left"><div class="avatar avatar-sm"><strong>' + $('.ui-picker-selected:first .ui-picker-draggable-avatar strong').html() + '</strong></div></div><div class="tile-inner text-overflow">' + $('.ui-picker-selected:first .ui-picker-info-title').html() + '</div></div>');
-					},
-					start: function (event, ui) {
-						var draggableCount = $('.ui-picker-selected').length;
+	if (typeof jQuery.ui != 'undefined') {
+		// draggable
+			$('.ui-picker-draggable-handler').draggable({
+				addClasses: false,
+				appendTo: 'body',
+				cursor: 'move',
+				cursorAt: {
+					top: 0,
+					left: 0 
+				},
+				delay: 100,
+				helper: function () {
+					return $('<div class="tile tile-brand-accent ui-draggable-helper"><div class="tile-side pull-left"><div class="avatar avatar-sm"><strong>' + $('.ui-picker-selected:first .ui-picker-draggable-avatar strong').html() + '</strong></div></div><div class="tile-inner text-overflow">' + $('.ui-picker-selected:first .ui-picker-info-title').html() + '</div></div>');
+				},
+				start: function (event, ui) {
+					var draggableCount = $('.ui-picker-selected').length;
 
-						if (draggableCount > 1) {
-							$('.ui-draggable-helper').append('<div class="avatar avatar-brand avatar-sm ui-picker-draggable-count">' + draggableCount + '</div>');
-						};
-					},
-					zIndex: 100
-				});
+					if (draggableCount > 1) {
+						$('.ui-draggable-helper').append('<div class="avatar avatar-brand avatar-sm ui-picker-draggable-count">' + draggableCount + '</div>');
+					};
+				},
+				zIndex: 100
+			});
 
-			// droppable
-				$('.ui-picker-nav .nav a').droppable({
-					accept: '.ui-picker-draggable-handler',
-					addClasses: false,
-					drop: function(event, ui) {
-						$('body').snackbar({
-							content: 'Dropped on "' + $(this).html() + '"'
-						});
-					},
-					hoverClass: 'ui-droppable-helper',
-					tolerance: 'pointer'
-				});
+		// droppable
+			$('.ui-picker-nav .nav a').droppable({
+				accept: '.ui-picker-draggable-handler',
+				addClasses: false,
+				drop: function(event, ui) {
+					$('body').snackbar({
+						content: 'Dropped on "' + $(this).html() + '"'
+					});
+				},
+				hoverClass: 'ui-droppable-helper',
+				tolerance: 'pointer'
+			});
 
-			// selectable
-				$pickerLib.selectable({
-					cancel: '.ui-picker-draggable-handler',
-					filter: '.ui-picker-selectable-handler',
-					selecting: function (event, ui) {
-						var $selectingParent = $(ui.selecting).parent();
+		// selectable
+			$pickerLib.selectable({
+				cancel: '.ui-picker-draggable-handler',
+				filter: '.ui-picker-selectable-handler',
+				selecting: function (event, ui) {
+					var $selectingParent = $(ui.selecting).parent();
 
-						$selectingParent.addClass('tile-brand-accent ui-picker-selected');
+					$selectingParent.addClass('tile-brand-accent ui-picker-selected');
 
-						$('.ui-picker-info').addClass('ui-picker-info-active').removeClass('ui-picker-info-null');
-						$('.ui-picker-info-desc-wrap').html($selectingParent.find('.ui-picker-info-desc').html());
-						$('.ui-picker-info-title-wrap').html($selectingParent.find('.ui-picker-info-title').html());
+					$('.ui-picker-info').addClass('ui-picker-info-active').removeClass('ui-picker-info-null');
+					$('.ui-picker-info-desc-wrap').html($selectingParent.find('.ui-picker-info-desc').html());
+					$('.ui-picker-info-title-wrap').html($selectingParent.find('.ui-picker-info-title').html());
 
-						var pickerMapLatLng = new google.maps.LatLng($selectingParent.find('.ui-picker-info-map-lat').html(), $selectingParent.find('.ui-picker-info-map-lng').html());
+					var pickerMapLatLng = new google.maps.LatLng($selectingParent.find('.ui-picker-info-map-lat').html(), $selectingParent.find('.ui-picker-info-map-lng').html());
 
-						pickerMap.setCenter(pickerMapLatLng);
+					pickerMap.setCenter(pickerMapLatLng);
+					pickerMarker.setMap(pickerMap);
+					pickerMarker.setPosition(pickerMapLatLng);
+				},
+				unselecting: function (event, ui) {
+					var $unselectingParent = $(ui.unselecting).parent();
+
+					$unselectingParent.removeClass('tile-brand-accent ui-picker-selected');
+
+					if (!$('.ui-picker-selected').length) {
+						$('.ui-picker-info').addClass('ui-picker-info-null');
+						$('.ui-picker-info-desc-wrap').html('');
+						$('.ui-picker-info-title-wrap').html('');
+						pickerMarker.setMap(null);
+					} else {
+						var $first = $($('.ui-picker-selected')[0]);
+
+						$('.ui-picker-info-desc-wrap').html($first.find('.ui-picker-info-desc').html());
+						$('.ui-picker-info-title-wrap').html($first.find('.ui-picker-info-title').html());
+
+						var firstLatLng = new google.maps.LatLng($first.find('.ui-picker-info-map-lat').html(), $first.find('.ui-picker-info-map-lng').html());
+
+						pickerMap.setCenter(firstLatLng);
 						pickerMarker.setMap(pickerMap);
-						pickerMarker.setPosition(pickerMapLatLng);
-					},
-					unselecting: function (event, ui) {
-						var $unselectingParent = $(ui.unselecting).parent();
+						pickerMarker.setPosition(firstLatLng);
+					};
+				}
+			});
+	};
 
-						$unselectingParent.removeClass('tile-brand-accent ui-picker-selected');
-
-						if (!$('.ui-picker-selected').length) {
-							$('.ui-picker-info').addClass('ui-picker-info-null');
-							$('.ui-picker-info-desc-wrap').html('');
-							$('.ui-picker-info-title-wrap').html('');
-							pickerMarker.setMap(null);
-						} else {
-							var $first = $($('.ui-picker-selected')[0]);
-
-							$('.ui-picker-info-desc-wrap').html($first.find('.ui-picker-info-desc').html());
-							$('.ui-picker-info-title-wrap').html($first.find('.ui-picker-info-title').html());
-
-							var firstLatLng = new google.maps.LatLng($first.find('.ui-picker-info-map-lat').html(), $first.find('.ui-picker-info-map-lng').html());
-
-							pickerMap.setCenter(firstLatLng);
-							pickerMarker.setMap(pickerMap);
-							pickerMarker.setPosition(firstLatLng);
-						};
-					}
-				});
-		};
-
-	// picker info
-		$(document).on('click', '.ui-picker-info-close', function () {
-			$('.ui-picker-info').removeClass('ui-picker-info-active');
-		});
+	$(document).on('click', '.ui-picker-info-close', function () {
+		$('.ui-picker-info').removeClass('ui-picker-info-active');
+	});
 
 // ui-picker.html
 	$('#doc_datepicker_example_1').pickdate();
