3dbf98cc9976f3f5503a35d97e08ba85d69627a4
Hexhu <huangxu00123@gmail.com>
2016-08-26 17:26:40 +0900

    sha1 test
    may used for user auth purpose

---
+++ action/sha1.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title> SHA1 </title>
+</head>
+<?php
+shaz(H19);
+shaz(J19);
+shaz(L19);
+shaz(M19);
+shaz(N19);
+shaz(O19);
+shaz(P19);
+shaz(Q19);
+shaz(R19);
+shaz(S19);
+shaz(L18);
+shaz(M18);
+shaz(N18);
+shaz(O18);
+shaz(P18);
+shaz(N17);
+shaz(P17);
+shaz(O16);
+shaz(R16);
+shaz(N15);
+shaz(O14);
+
+function shaz($var) {
+$sha = '新葡京ABCHIL17311' + $var;
+$shaa = sha1($sha);
+echo $sha . ':' . strtoupper($shaa) . '<br><br>';
+}
+?>
+</html>
