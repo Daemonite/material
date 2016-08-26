de5cfc8143322487ffef83b5e1a9a836d101b026
Hexhu <huangxu00123@gmail.com>
2016-08-26 17:48:33 +0900

    sha1 v2
    corrected the answer of Q2

--- action/sha1.php
+++ action/sha1.php
@@ -4,32 +4,12 @@
     <title> SHA1 </title>
 </head>
 <?php
-shaz(H19);
-shaz(J19);
-shaz(L19);
-shaz(M19);
-shaz(N19);
-shaz(O19);
-shaz(P19);
-shaz(Q19);
-shaz(R19);
-shaz(S19);
-shaz(L18);
 shaz(M18);
-shaz(N18);
-shaz(O18);
-shaz(P18);
-shaz(N17);
-shaz(P17);
-shaz(O16);
-shaz(R16);
-shaz(N15);
-shaz(O14);
 
 function shaz($var) {
-$sha = '新葡京ABCHIL17311' + $var;
+$sha = '新葡京ABCHI17311' . $var;
 $shaa = sha1($sha);
-echo $sha . ':' . strtoupper($shaa) . '<br><br>';
+echo $sha . ':<br>' . strtoupper($shaa) . '<br><br>';
 }
 ?>
 </html>
