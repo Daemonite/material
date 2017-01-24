f477e85ac426de532c724e911409f41a63f1e1c5
MAYA Ma <sesemaya@gmail.com>
2017-01-25 09:40:41 +1100

    recompile css and js

--- js/material.js
+++ js/material.js
@@ -304,7 +304,7 @@ var FloatingLabel = function ($) {
 
 /*!
  * navigation drawer
- * based on bootstrap's (v4.0.0-alpha.5) modal.js
+ * based on bootstrap's (v4.0.0-alpha.6) modal.js
  */
 var NavDrawer = function ($) {
   // constants >>>
@@ -659,7 +659,7 @@ var NavDrawer = function ($) {
 
 /*!
  * tab indicator animation
- * requires bootstrap's (v4.0.0-alpha.5) tab.js
+ * requires bootstrap's (v4.0.0-alpha.6) tab.js
  */
 var TabSwitch = function ($) {
   // constants >>>
@@ -800,7 +800,7 @@ var TabSwitch = function ($) {
 
 /*!
  * global util js
- * based on bootstrap's (v4.0.0-alpha.5) util.js
+ * based on bootstrap's (v4.0.0-alpha.6) util.js
  */
 var Util = function ($) {
   var transition = false;
