070f9db7f6b805c2c0fb6580062056056c84c853
MAYA Ma <sesemaya@gmail.com>
2017-02-10 11:33:36 +1100

    doc js: add clipboard.js

--- assets/js-project/project.js
+++ assets/js-project/project.js
@@ -1,13 +1,6 @@
 $(function () {
 
   // bootstrap doc js
-    Holder.addTheme('gray', {
-      bg         : '#757575',
-      fg         : 'rgba(255, 255, 255, 0.7)',
-      font       : 'Helvetica',
-      fontweight : 'normal'
-    });
-
     // disable empty links in doc
       $('.bd-content [href="#"]').click(function (e) {
         e.preventDefault();
@@ -16,7 +9,7 @@ $(function () {
     // form: indeterminate checkbox
       $('.bd-example-indeterminate [type="checkbox"]').prop('indeterminate', true);
 
-    // modal
+    // modal: related target demo
       $('#exampleModal').on('show.bs.modal', function (event) {
         var $button   = $(event.relatedTarget);
         var $modal    = $(this);
@@ -26,7 +19,7 @@ $(function () {
         $modal.find('.modal-body input').val(recipient);
       });
 
-    // progress
+    // progress: activate animated progress bar
       $('.bd-toggle-animated-progress').on('click', function () {
         $(this).siblings('.progress').find('.progress-bar-striped').toggleClass('progress-bar-animated');
       });
@@ -37,6 +30,51 @@ $(function () {
 
       $('.popover-test').popover();
       $('.tooltip-test').tooltip();
+    
+    // vendor: clipboard.js
+      $('.highlight').each(function () {
+        var btnHtml = '<div class="bd-clipboard"><span class="btn-clipboard" title="Copy to clipboard">Copy</span></div>';
+
+        $(this).before(btnHtml);
+        $('.btn-clipboard').tooltip();
+      });
+
+      var clipboard = new Clipboard('.btn-clipboard', {
+        target: function (trigger) {
+          return trigger.parentNode.nextElementSibling;
+        }
+      });
+
+      clipboard.on('success', function (e) {
+        $(e.trigger)
+          .attr('title', 'Copied!')
+          .tooltip('_fixTitle')
+          .tooltip('show')
+          .attr('title', 'Copy to clipboard')
+          .tooltip('_fixTitle');
+
+        e.clearSelection();
+      });
+
+      clipboard.on('error', function (e) {
+        var modifierKey = /Mac/i.test(navigator.userAgent) ? '\u2318' : 'Ctrl-';
+        var fallbackMsg = 'Press ' + modifierKey + 'C to copy';
+
+        $(e.trigger)
+          .attr('title', fallbackMsg)
+          .tooltip('_fixTitle')
+          .tooltip('show')
+          .attr('title', 'Copy to clipboard')
+          .tooltip('_fixTitle');
+      });
+
+    // vendor: holder.js
+      Holder.addTheme('gray', {
+        bg         : '#757575',
+        fg         : 'rgba(255, 255, 255, 0.7)',
+        font       : 'Helvetica',
+        fontweight : 'normal'
+      });
 
   // material pickers
     $('#exampleInputDatePicker1').pickdate();
