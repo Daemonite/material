4721d11e1946f7735553729543e886e2863b032c
MAYA <sesemaya@gmail.com>
2017-11-15 14:55:50 +1100

    Install cssnano

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,14 +1,19 @@
 'use strict'
 
 module.exports = (ctx) => ({
-  map: ctx.file.dirname.includes('examples') ? false : {
+  map: {
     annotation: true,
     inline: false,
     sourcesContent: true
   },
   plugins: {
-    autoprefixer: {
+    autoprefixer: ctx.env === 'prefix' ? {
       cascade: false
-    }
+    } : false,
+    cssnano: ctx.env === 'minify' ? {
+      "preset": [
+        "default"
+      ]
+    } : false
   }
 })
